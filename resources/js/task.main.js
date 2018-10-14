adaa329944ddd661f2c43fd1f50a80cda41713b8
Md. Rakibul Hasan Molla <six.rakib@gmail.com>
2018-10-14 22:47:57 +0600

    part 0

---
+++ resources/js/task.main.js
@@ -0,0 +1,70 @@
+
+function showHint(str,id) {
+  var email = document.getElementById("taskEamil");
+
+     email.setCustomValidity("");
+
+
+
+    if (str.length == 0) { 
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    }else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","assign_task_ajax.php?ajax=1&&searchName="+str+"&&taskId="+id, true);
+        xmlhttp.send();
+    }
+}
+
+function validTask() {
+    
+     var st = document.getElementById("txtHint").innerText;
+     var email = document.getElementById("taskEamil");
+
+     //email.setCustomValidity("");
+      //st =
+      if(st.trim() == "Searching...")
+      {
+       // console.log(st);
+         email.setCustomValidity("Invalid Email");
+
+      }
+      else if (st.includes("Already Assigned"))
+      {
+          email.setCustomValidity("Already Assigned");
+      }
+              
+}
+
+function goLastMonth(month,year)
+{
+    if(month==1)
+      {
+        --year;
+        month=13;
+      }
+    document.location.href="?month="+(month-1)+"&year="+year;
+         
+}
+
+function goNextMonth(month,year){
+    if(month==12)
+      {
+        ++year;
+        month=0;
+      }
+    document.location.href="?month="+(month+1)+"&year="+year;
+         
+}
+
+function goLastDay(day,month,year)
+{
+    
+    document.location.href="?showtask="+year+"-"+month+"-"+day;
+         
+}
